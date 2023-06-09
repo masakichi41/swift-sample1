930124013e95591e4b42cbdb679e869b82869cf8
masakichi41 <86694940+masakichi41@users.noreply.github.com>
2023-06-09 21:20:15 +0900

    Update WindowController.swift

--- sample/WindowController.swift
+++ sample/WindowController.swift
@@ -10,7 +10,7 @@ class WindowController: NSWindowController, NSWindowDelegate {
     @IBOutlet weak var toolbarItem: NSToolbarItem!
 
     func windowWillResize(_ sender: NSWindow, to frameSize: NSSize) -> NSSize {
-        // If I uncomment this below it works as expected but I get the warning.
+        // Uncommenting the following code will make it work properly, but a warning will be displayed.
         // toolbarItem.minSize.width = frameSize.width - 86
         return frameSize
     }
